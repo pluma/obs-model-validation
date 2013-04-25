5de2211070ca8e076a1ffd2d094ef26f8cd276d6
pluma <me@pluma.io>
2013-04-25 03:01:19 +0200

    Major overhaul.

--- README.md
+++ README.md
@@ -5,38 +5,7 @@ Model validation plugin for [observable models](https://github.com/pluma/obs-mod
 # Basic usage example
 
 ```javascript
-var User = model('User')
-    .use(validation),
-    .attr('id', {validate: /^[1-9][0-9]+$/})
-    .attr('username', {
-        validate: function(v) {
-            return v && v.toLowerCase && v === v.toLowerCase();
-        }
-    });
-
-var admin = new User({id: 1, username: 'admin'});
-console.log(admin.valid()); // true
-console.log(admin.id(), admin.id.valid()); // 1, true
-console.log(admin.username(), admin.username.valid()); // "admin", true
-
-admin.valid.subscribe(function(valid) {
-    console.log('Admin user is now ' + (valid ? 'valid' : 'invalid'));
-});
-
-admin.id(0);
-// -> "Admin user is now invalid"
-console.log(admin.id(), admin.id.valid()); // 0, false
-console.log(admin.valid()); // false
-
-admin.id(1);
-// -> "Admin user is now valid"
-console.log(admin.id(), admin.id.valid()); // 1, true
-console.log(admin.valid()); // true
-
-admin.username('ADMIN');
-// -> "Admin user is now invalid"
-console.log(admin.username(), admin.username.valid()); // "ADMIN", false
-console.log(admin.valid()); // false
+// TODO
 ```
 
 # API
@@ -47,18 +16,106 @@ The validation plugin. Use with `Model.use(validation)`.
 
 ## Model.attr(name, options)
 
-The `options` argument is expanded to allow an optional property `validate`.
+The `options` argument is expanded to allow defining validation requirements for each field.
 
-The value can be either a regular expression or a function which will receive the attribute value whenever it changes.
+### options.required (Default: false)
 
-The value can also be an object with the keys `fn` and `lazy`, with `fn` containing either a regular expression or a function and `lazy` being an optional boolean. If `lazy` is set to `True`, the validation will not be applied automatically whenever the value changes but only when it is accessed directly.
+Defines whether the attribute is required. If the attribute is required, it will fail validation if it is set to `null` or `undefined`. If the attribute is not required, a value of `null` or `undefined` will exempt the attribute from further validation and mark it as valid.
 
-**NOTE**: If a model uses any lazy validations, the model's `dirty` property will only reflect changes to lazy `dirty` fields when they are accessed directly.
+### options.values:Array
 
-## Model#[attrName].dirty
+Defines an array of possible values that will exempt the attribute from further validation (resulting in it being marked as valid without having to pass any further validations defined on the attribute).
 
-Each validated attribute receives an observable property `dirty` which indicates whether the property passed validation.
+**NOTE**: Uses [jkroso's equals 0.2.0](https://github.com/jkroso/equals/tree/0.2.0) for value comparison.
 
-## Model#dirty
+### options.type
 
-Each model instance receives an observable property `dirty` which indicates whether all validated properties passed validation.
+Defines a type or an array of types the attribute value is expected to match. A type can be defined either as the string return value of a `typeof` check, a constructor function the value is expected to be an `instanceof` or the special string value `integer` which will match any whole `number` (including `Infinity`).
+
+If the attribute value does not match any of the expected types, the attribute will fail validation.
+
+### options.lengthRange:Integer
+
+Defines a range the length of the attribute value is expected to match. If the value has no `length` property or the value of its `length` property is not a `number`, it will always fail this validation.
+
+#### options.lengthRange.min:Integer
+
+Defines the expected minimum length of the attribute value. If the length of the attribute value is less than `min`, it will fail validation.
+
+#### options.lengthRange.max:Integer
+
+Defines the expected maximum length of the attribute value. If the length of the attribute value is greater than `max`, it will fail validation.
+
+### options.valueRange
+
+Defines a range the attribute value is expected to match. If the value is not a `number`, it will always fail this validation.
+
+#### options.valueRange.min:Number and options.valueRange.minExclusive:Boolean
+
+Defines the expected minimum attribute value. If the attribute value is less than `min`, it will fail validation. If `minExclusive` is set to `true`, the attribute value will also fail if it is equal to `min`.
+
+#### options.valueRange.max:Number and options.valueRange.maxExclusive:Boolean
+
+Defines the expected maximum attribute value. If the attribute value is greater than `max`, it will fail validation. If `maxExclusive` is set to `true`, the attribute value will also fail if it is equal to `max`.
+
+#### options.valueRange.step:Number
+
+Defines a number the attribute value is expected to be a multiple of, offset by the smallest possible integer matching the `min` and `minExclusive` requirements, or `0` if `min` is not set.
+
+For example: if `step` is set to `2` and `min` is set to `1` (`minExclusive` is not set or is set to `false`), the attribute value is expected to match `x * 2 + 1`, i.e. `1, 3, 5, 7, …`.
+
+#### options.valueRange.multipleOf:Number
+
+Defines a number the attribute value is expected to be a multiple of (offset by `0`, rather than `min`).
+
+### options.pattern:RegExp
+
+A regular expression the attribute value is expected to be tested successfully against. If `pattern` is a string, it will be converted into a regular expression when the model instance is created. If [XRegExp](https://github.com/slevithan/xregexp) is available, string values will be converted using that library instead.
+
+**NOTE**: Although `pattern` SHOULD be set to a regular expression or a full string representation of a regular expression, any object that has a `test` method should behave as expected.
+
+### options.validate:Function
+
+Defines a custom validation function the attribute value will be passed to. The function will be bound to the model instance and should return `false` if the value has failed validation or `true` if the validation was successful.
+
+Alternatively, the function can return `null`, indicating the validation has failed tentatively, or the special value `validation.EXEMPT` (equivalent to the string value `exempt`) if the attribute should be exempted from further validation and marked valid. See below for further details.
+
+## Model.validation(fn, attrs)
+
+Defines a multi-attribute validation function and an array of attribute names it affects. The validation function will be passed the current value of each attribute in the order specified whenever the value of one of the attributes changes and is bound to the model instance.
+
+The referenced attributes have to exist on the model instance at the time of its creation.
+
+Multi-attribute validations executed in the order they are added to the model, but will only be executed after all regular attribute validations have passed and the attribute value is not exempt from further validation.
+
+The validation function should return `true`, `false`, `null` or `validation.EXEMPT` (see below).
+
+## Model#[attrName].valid
+
+Every attribute receives an observable property `valid` which indicates whether the attribute value passed validation.
+
+## Model#valid
+
+Each model instance receives an observable property `valid` which indicates whether all attributes passed validation.
+
+## Validation function return values
+
+Attribute validation functions and mult-attribute validation functions MUST return one of the following values. If multiple validation functions exist for the same attribute (e.g. because the attribute is affected by other multi-attribute validations), they will be executed in the order they were defined on the model, with regular attribute validation functions being executed before multi-attribute validations affecting the same attribute.
+
+### `true`
+
+The validation was tentatively successful. Any pending validations on the same attribute will be executed normally. If this was the last validation to be executed on this attribute, the attribute will be marked as *valid*.
+
+### `false`
+
+The validation has failed. Any pending validations on the same attribute will be ignored and the attribue will be marked as *invalid*.
+
+### `null`
+
+The validation has tentatively failed. Any pending validations on the same attribute will be executed normally. If this was the last validation to be executed on this attribute, the attribute will be marked as *invalid*.
+
+### `validation.EXEMPT`
+
+The validation was successful. Any pending validations on the same attribute will be ignored and the attribute will be marked as *valid*.
+
+**NOTE**: The string value `exempt` can be used instead. This value is available as a property on the `validation` plugin to avoid sneaky typos.
--- dist/obs-model-validation.amd.js
+++ dist/obs-model-validation.amd.js
@@ -1,61 +1,243 @@
 /*! obs-model-validation 0.1.0 Copyright (c) 2013 Alan Plum. MIT licensed. */
 define(function(require, exports) {
-var aug = require('aug'),
-    obs = require('obs');
-
-function validation() {
-    var dfn, prop;
-
-    this.valid = obs.computed((function(that) {
-        return function() {
-            for (var key in that.model.attrs) {
-                if (that[key].valid && !that[key].valid()) {
-                    return false;
-                }
-            }
-            return true;
-        };
-    }(this)));
-
-    for (var key in this.model.attrs) {
-        dfn = this.model.attrs[key];
-
-        if (!dfn || !dfn.validate) {
-            continue;
-        }
-
-        prop = this[key];
-        prop.valid = (
-            dfn.validate.lazy ? obs.computed.lazy : obs.computed
-        )(validation.createValidator(
-            dfn.validate.fn ? dfn.validate.fn : dfn.validate, prop
-        ));
-
-        prop.valid.watch(prop);
-        this.valid.watch(prop.valid);
-        this._destructors.push(validation.createDestructor(prop, this.valid));
+function optionallyRequire(name, fallback) {
+    try {
+        return require(name);
+    } catch(e) {
+        return fallback;
     }
 }
 
-aug(validation, {
-    createValidator: function(validator, prop) {
-        if (typeof validator === 'function') {
-            return function() {
-                return validator(prop());
-            };
+
+var assimilate = require('assimilate'),
+    equals = require('equals'),
+    obs = require('obs'),
+    XRegExp = optionallyRequire('xregexp', RegExp),
+    isArray = Array.isArray ? Array.isArray : function(arr) {
+        return Object.prototype.toString.call(arr) === '[object Array]';
+    };
+
+if (XRegExp.XRegExp) {
+    // xregexp < 3.0
+    XRegExp = XRegExp.XRegExp;
+}
+
+
+function validation(model) {
+    var key, dfn, prop;
+
+    model.valid = obs.computed(function() {
+        for (var key in model.model.attrs) {
+            if (!model[key].valid()) {
+                return false;
+            }
         }
-        if (validator instanceof RegExp) {
-            return function() {
-                return validator.test(prop());
-            };
+        return true;
+    });
+
+    for (key in model.model.attrs) {
+        dfn = model.model.attrs[key] || {};
+
+        prop = model[key];
+        prop.valid = obs.computed({
+            read: validation.createValidator(dfn, prop),
+            context: model
+        });
+
+        prop.valid.watch(prop);
+        model.valid.watch(prop.valid);
+        model._destructors.push(validation.createDestructor(prop, model.valid));
+    }
+
+    for (key in model.model.validations) {
+        dfn = model.model.validations[key];
+        for (var i = 0; i < dfn.props.length; i++) {
+            prop = model[dfn.props[i]];
+            prop.valid.read.validations.push(
+                validation.createModelValidation(dfn, model)
+            );
+            for (var j = 0; j < dfn.props.length; j++) {
+                if (i === j) {
+                    continue;
+                }
+                prop.valid.watch(model[dfn.props[j]]);
+            }
         }
-        throw new Error('Unknown validation type!');
+    }
+
+    for (key in model.model.attrs) {
+        model[key].notify();
+    }
+
+    model._destructors.push(function() {
+        for (var key in model.model.validations) {
+            var dfn = model.model.validations[key];
+            for (var i = 0; i < dfn.props.length; i++) {
+                var prop = model[dfn.props[i]];
+                for (var j = 0; j < dfn.props.length; j++) {
+                    if (i === j) {
+                        continue;
+                    }
+                    prop.valid.unwatch(model[dfn.props[j]]);
+                }
+            }
+        }
+    });
+}
+
+assimilate(validation, {
+    EXEMPT: 'exempt',
+    createValidator: function(dfn, prop) {
+        var validations = [];
+
+        validations.push(function(v) {
+            if (v === null || v === undefined) {
+                return dfn.required ? false : validation.EXEMPT;
+            }
+            return true;
+        });
+
+        if (dfn.values) {
+            validations.push(function(v) {
+                for (var i = 0; i < dfn.values.length; i++) {
+                    if (equals(v, dfn.values[i])) {
+                        return validation.EXEMPT;
+                    }
+                }
+                return null; // tentative failure
+            });
+        }
+
+        if (dfn.type) {
+            validations.push(function(v) {
+                if (isArray(dfn.type)) {
+                    for (var i = 0; i < dfn.type.length; i++) {
+                        if (validation.hasType(dfn.type[i], v)) {
+                            return true;
+                        }
+                    }
+                    return false;
+                } else {
+                    return validation.hasType(dfn.type, v);
+                }
+            });
+        }
+
+        if (dfn.lengthRange) {
+            validations.push(function(v) {
+                if (typeof v.length !== 'number') {
+                    return false;
+                }
+                if (dfn.lengthRange.min !== undefined) {
+                    if (v.length < dfn.lengthRange.min) {
+                        return false;
+                    }
+                }
+                if (dfn.lengthRange.max !== undefined) {
+                    if (v.length > dfn.lengthRange.max) {
+                        return false;
+                    }
+                }
+                return true;
+            });
+        }
+
+        if (dfn.valueRange) {
+            validations.push(function(v) {
+                var minValue = 0;
+                if (typeof v !== 'number') {
+                    return false;
+                }
+                if (dfn.valueRange.min !== undefined) {
+                    minValue = dfn.valueRange.min;
+                    if (dfn.valueRange.minExclusive) {
+                        minValue += 1;
+                        if (v <= dfn.valueRange.min) {
+                            return false;
+                        }
+                    } else if (v < dfn.valueRange.min) {
+                        return false;
+                    }
+                }
+                if (dfn.valueRange.max !== undefined) {
+                    if (dfn.valueRange.maxExclusive) {
+                        if (v >= dfn.valueRange.max) {
+                            return false;
+                        }
+                    } else if (v > dfn.valueRange.max) {
+                        return false;
+                    }
+                }
+                if (dfn.valueRange.step !== undefined) {
+                    return (v - minValue) % dfn.valueRange.step === 0;
+                }
+                if (dfn.valueRange.multipleOf !== undefined) {
+                    return v % dfn.valueRange.multipleOf === 0;
+                }
+                return true;
+            });
+        }
+
+        if (dfn.pattern) {
+            var pattern = (
+                typeof dfn.pattern === 'string' ?
+                new XRegExp(dfn.pattern) :
+                dfn.pattern
+            );
+            validations.push(function(v) {
+                return pattern.test(v);
+            });
+        }
+
+        if (dfn.validate) {
+            validations.push(dfn.validate);
+        }
+
+        return assimilate(function() {
+            var value = prop(),
+                result = true;
+
+            for (var i = 0; i < validations.length; i++) {
+                result = validations[i].call(this, value);
+                if (result === validation.EXEMPT || result === false) {
+                    return result === validation.EXEMPT;
+                }
+            }
+            return result === true;
+        }, {validations: validations});
+    },
+    createModelValidation: function(dfn, model) {
+        return function() {
+            var values = [];
+            for (var i = 0; i < dfn.props.length; i++) {
+                values.push(model[dfn.props[i]]());
+            }
+            return dfn.validate.apply(model, values);
+        };
     },
     createDestructor: function(prop, valid) {
         return function() {
             prop.valid.unwatch(prop);
             valid.unwatch(prop.valid);
         };
+    },
+    contributeToModel: function(Model) {
+        assimilate(Model, {
+            validations: [],
+            validation: function(validate, props) {
+                this.validations.push({validate: validate, props: props});
+                return this;
+            }
+        });
+    },
+    hasType: function(type, v) {
+        if (type === 'integer') {
+            return typeof v === 'number' && v === Math.floor(v);
+        } else if (typeof type === 'string') {
+            return typeof v === type;
+        } else {
+            return v instanceof type;
+        }
     }
 });
 
